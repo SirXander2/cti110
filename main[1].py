1f481181551a38a4f6ae0da4ac10f626cde0ee98
SirXander2 <91159347+SirXander2@users.noreply.github.com>
2021-10-29 22:42:18 -0400

    Add files via upload
    Homework Interstate exercise

--- main[1].py
+++ main[1].py
@@ -1,8 +1,18 @@
-caffeine_mg = float(input())
-first_half=caffeine_mg // 2
-print('After 6 hours: {:.2f} mg'.format(first_half))
-second_half=first_half // 2
-print('After 12 hours: {:.2f} mg'.format(second_half))
-third_half=second_half / 4
-print('After 24 hours: {:.2f} mg'.format(third_half))
+highway_number = int(input())
 
+if ((highway_number < 1) or (highway_number > 999) or ((highway_number % 100) == 0)): # Invalid
+
+    print('{} is not a valid interstate highway number.'.format(highway_number))
+
+else:
+    if highway_number >99:
+        primary = highway_number % 100
+        print('I-',highway_number,' is auxiliary, serving I-',primary,',', sep='',end='')
+
+    else:
+        print('I-',highway_number, ' is primary,', sep='', end='')
+    if highway_number %2==0:
+        print(' going east/west.' )
+    else:
+        print(' going north/south.')
+   
