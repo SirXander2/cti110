d45d9167538db898479096e427fa11afff8a3d39
SirXander2 <91159347+SirXander2@users.noreply.github.com>
2021-10-29 22:40:01 -0400

    Add files via upload
    Caffeine Homework

---
+++ main[1].py
@@ -0,0 +1,8 @@
+caffeine_mg = float(input())
+first_half=caffeine_mg // 2
+print('After 6 hours: {:.2f} mg'.format(first_half))
+second_half=first_half // 2
+print('After 12 hours: {:.2f} mg'.format(second_half))
+third_half=second_half / 4
+print('After 24 hours: {:.2f} mg'.format(third_half))
+
