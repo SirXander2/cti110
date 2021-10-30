b21bb1bafc385bef0c91a9b924e3b0d16c89fee2
SirXander2 <91159347+SirXander2@users.noreply.github.com>
2021-10-29 22:43:57 -0400

    Add files via upload
    Interleaved Homework

--- main[1].py
+++ main[1].py
@@ -1,18 +1,5 @@
-highway_number = int(input())
+print('Enter x: ')
+x = int(input())
 
-if ((highway_number < 1) or (highway_number > 999) or ((highway_number % 100) == 0)): # Invalid
-
-    print('{} is not a valid interstate highway number.'.format(highway_number))
-
-else:
-    if highway_number >99:
-        primary = highway_number % 100
-        print('I-',highway_number,' is auxiliary, serving I-',primary,',', sep='',end='')
-
-    else:
-        print('I-',highway_number, ' is primary,', sep='', end='')
-    if highway_number %2==0:
-        print(' going east/west.' )
-    else:
-        print(' going north/south.')
-   
+ # Student mistakenly is echoing the input to output to match example
+print('x doubled is:', (2 * x))
